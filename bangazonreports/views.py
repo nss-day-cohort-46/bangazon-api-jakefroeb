c05e1c452f859c8239300504a1c4e1148755b7ba
jakefroeb <jake.herman.froeb@gmail.com>
2021-06-02 15:44:27 -0500

    issue 5

--- bangazonreports/views.py
+++ bangazonreports/views.py
@@ -6,6 +6,12 @@ from bangazonapi.models import Product
 def expensive_product_list(request):
     if request.method == "GET":
         products = Product.objects.filter(price__gte=1000)
-        template = 'products/expensive_products.html'
+        template = 'products/products.html'
+        context = {'products' : products}
+        return render(request, template, context)
+def inexpensive_product_list(request):
+    if request.method == "GET":
+        products = Product.objects.filter(price__lt=1000)
+        template = 'products/products.html'
         context = {'products' : products}
         return render(request, template, context)
