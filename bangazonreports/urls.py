8fa15218e67277d762d3fda3509a9a5f8aa480e7
jakefroeb <jake.herman.froeb@gmail.com>
2021-06-03 15:19:30 -0500

    issue 2

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import expensive_product_list, inexpensive_product_list, incomplete_order_list
+from .views import expensive_product_list, inexpensive_product_list, incomplete_order_list, favorited_sellers
 
 urlpatterns = [
     path('reports/expensive_products', expensive_product_list),
     path('reports/inexpensive_products', inexpensive_product_list),
-    path('reports/incomplete_orders', incomplete_order_list)
+    path('reports/incomplete_orders', incomplete_order_list),
+    path('reports/favorite_sellers', favorited_sellers)
+
 ]
--- bangazonreports/views.py
+++ bangazonreports/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from bangazonapi.models import Product, Order
+from bangazonapi.models import Product, Order, Customer, Favorite
 from django.db.models import Sum
 
 def expensive_product_list(request):
@@ -21,4 +21,15 @@ def incomplete_order_list(request):
         orders = Order.objects.filter(payment_type=None).annotate(total = Sum('lineitems__product__price'))
         template = 'orders/orders.html'
         context = {'orders' : orders}
+        return render(request, template, context)
+
+def favorited_sellers(request):
+    if request.method == "GET":
+        favorites = Favorite.objects.all()
+        customers = Customer.objects.filter(favorite__isnull = False).distinct()
+        for customer in customers:
+            favorited_sel = customer.favorite_set.all()
+            customer.favorites = favorited_sel
+        template = 'customers/customers.html'
+        context = {'customers' : customers}
         return render(request, template, context)
